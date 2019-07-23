f80de262ba13a202f5c32b84ac7217ca8e5bee23
Rubén Segura <rsegrom@gmail.com>
2019-07-23 17:28:19 +0200

    añadidas rutas hijas de usuarios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,24 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UsuarioComponent } from './usuario/usuario/usuario.component';
+import { UsuarioNuevoComponent } from './usuario/usuario/usuario-nuevo.component';
+import { UsuarioEditarComponent } from './usuario/usuario/usuario-editar.component';
+import { UsuarioInformacionComponent } from './usuario/usuario/usuario-informacion.component';
 
 const ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'usuario', component: UsuarioComponent },
-    {path: '', pathMatch: 'full', redirectTo: 'home'},
-    {path: '**', pathMatch: 'full', redirectTo: 'home'}
+    { 
+        path: 'usuario/:id',
+        component: UsuarioComponent,
+        children: [
+            { path: 'añadir', component: UsuarioNuevoComponent },
+            { path: 'editar', component: UsuarioEditarComponent },
+            { path: 'ver', component: UsuarioInformacionComponent },
+            { path: '', pathMatch: 'full', redirectTo: 'ver' },
+        ]
+    },
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const APPROUTING = RouterModule.forRoot(ROUTES);
